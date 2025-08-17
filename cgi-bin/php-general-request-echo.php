483e1b0dbb7264f03885de0be93a67b436519aaa
Mianbao <gll004@ucsd.edu>
2025-08-17 15:47:26 -0700

    Add php general-request-echo file

---
+++ cgi-bin/php-general-request-echo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>General Request Echo</title>
+</head>
+<body>
+    <h1 style="text-align: center;">General Request Echo</h1>
+    <hr>
+    <?php 
+        echo '<p><b>HTTP Protocol: </b>' . $_SERVER['SERVER_PROTOCOL'] . '</p><br />'
+        echo '<p><b>HTTP Method: </b>' . $_SERVER['REQUEST_METHOD'] . '</p><br />'
+        echo '<p><b>Raw Query String: </b>' . $_SERVER['QUERY_STRING'] . '</p><br />'
+        echo '<p><b>Raw Message Body: </b>' . $_POST . '</p><br />'
+    ?>
+</body>
+</html>
