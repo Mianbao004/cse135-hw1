8990de3d76575f7cbdedbdf7f8b85f573301b3e7
Mianbao <gll004@ucsd.edu>
2025-08-17 15:48:51 -0700

    Fix PHP syntax

--- cgi-bin/php-general-request-echo.php
+++ cgi-bin/php-general-request-echo.php
@@ -9,10 +9,10 @@
     <h1 style="text-align: center;">General Request Echo</h1>
     <hr>
     <?php 
-        echo '<p><b>HTTP Protocol: </b>' . $_SERVER['SERVER_PROTOCOL'] . '</p><br />'
-        echo '<p><b>HTTP Method: </b>' . $_SERVER['REQUEST_METHOD'] . '</p><br />'
-        echo '<p><b>Raw Query String: </b>' . $_SERVER['QUERY_STRING'] . '</p><br />'
-        echo '<p><b>Raw Message Body: </b>' . $_POST . '</p><br />'
+        echo '<p><b>HTTP Protocol: </b>' . $_SERVER['SERVER_PROTOCOL'] . '</p><br />';
+        echo '<p><b>HTTP Method: </b>' . $_SERVER['REQUEST_METHOD'] . '</p><br />';
+        echo '<p><b>Raw Query String: </b>' . $_SERVER['QUERY_STRING'] . '</p><br />';
+        echo '<p><b>Raw Message Body: </b>' . $_POST . '</p><br />';
     ?>
 </body>
 </html>
