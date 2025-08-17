2a1677578c28bbed3009b41681f246201946b58d
Mianbao <gll004@ucsd.edu>
2025-08-17 15:59:04 -0700

    Fix message body

--- cgi-bin/php-general-request-echo.php
+++ cgi-bin/php-general-request-echo.php
@@ -9,10 +9,12 @@
     <h1 style="text-align: center;">General Request Echo</h1>
     <hr>
     <?php 
+        $raw_msg = file_get_contents('php://input');
+
         echo '<p><b>HTTP Protocol: </b>' . $_SERVER['SERVER_PROTOCOL'] . '</p><br />';
         echo '<p><b>HTTP Method: </b>' . $_SERVER['REQUEST_METHOD'] . '</p><br />';
         echo '<p><b>Raw Query String: </b>' . $_SERVER['QUERY_STRING'] . '</p><br />';
-        echo '<p><b>Raw Message Body: </b>' . $_POST . '</p><br />';
+        echo '<p><b>Raw Message Body: </b>' . $raw_msg . '</p><br />';
     ?>
 </body>
 </html>
