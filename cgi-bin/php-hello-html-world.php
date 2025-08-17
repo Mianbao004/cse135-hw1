dfd9b1c6ab4d3ada3ea7a832e320d862f8b583ee
Mianbao <gll004@ucsd.edu>
2025-08-16 17:38:50 -0700

    Add hello html php file

---
+++ cgi-bin/php-hello-html-world.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello PhP world!</title>
+</head>
+<body>
+    <h1>Hello HTML World with PhP</h1>
+    <?php
+        $name = "PhP";
+        echo "Hello HTML World from $name";
+
+        $date = date("l, F j, Y H:i:s");
+        echo "This program is generated at: $date";
+
+        $ip_address = $_SERVER['REMOTE_ADDR'];
+        echo "Your IP Address is: $ip_address";
+    ?>
+    
+</body>
+</html>
