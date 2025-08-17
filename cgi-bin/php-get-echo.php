789d96ae58e2930d15e2b18f82d11cd7b30ace3e
Mianbao <gll004@ucsd.edu>
2025-08-17 15:14:38 -0700

    Add parsed key-value pairs

--- cgi-bin/php-get-echo.php
+++ cgi-bin/php-get-echo.php
@@ -11,8 +11,16 @@
     <?php
         if ($_SERVER['REQUEST_METHOD'] === 'GET'){
             $q_str = $_SERVER['QUERY_STRING'];
-            echo '<p style="margin-bottom: 0;"><b>Query String:</b> ' . htmlspecialchars($q_str) . '</p> <br>';
-            echo htmlspecialchars($q_str) . '<br>';
+            echo '<b style="margin-bottom: 0;>Raw Query String:</b> ' . htmlspecialchars($q_str) . '<br />';
+            echo 'Formatted Query String: <br />';
+
+            parse_str($q_str, $params); //Stores parsed query string into an associative array
+    
+            echo '<ul>';
+            foreach ($params as $key => $value) {
+                echo '<li><b>' . htmlspecialchars($key) . '</b>: ' . htmlspecialchars($value) . '</li>';
+            }
+            echo '</ul>';
         } else {
             echo '<p><b>Error:</b> This page only accepts GET requests.</p>';
         }
