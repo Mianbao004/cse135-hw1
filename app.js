82601a1c2300b50d8cd34f4a168d698c8c9b72ad
Mianbao <gll004@ucsd.edu>
2025-08-19 14:58:21 -0700

    Fix routing path

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();                   // Initializes and returns an Express a
 
 app.set('trust proxy', true);            // To get correct client IP behind Apache via reverse proxy
 
-app.use('/cgi-bin/hello-html-world', helloHtmlWorld);
+app.use('/cgi-bin/js-helloHtmlWorld.js', helloHtmlWorld);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
