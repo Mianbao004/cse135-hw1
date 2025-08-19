a27a19272edd82a4a2e765be92be6ec44243c8b2
Mianbao <gll004@ucsd.edu>
2025-08-19 16:01:46 -0700

    Add server to listen for all network interfaces

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ import express from 'express';
 import helloHtmlWorld from './cgi-bin/js-helloHtmlWorld.js';
 
 const app = express();                   // Initializes and returns an Express application object
-
 app.set('trust proxy', true);            // To get correct client IP behind Apache via reverse proxy
-
 app.use('/cgi-bin/js-helloHtmlWorld.js', helloHtmlWorld);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
