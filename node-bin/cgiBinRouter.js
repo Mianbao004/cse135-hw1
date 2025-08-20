6afdbeb6291e4fb5e911819d8381e5c16a11a678
Mianbao <gll004@ucsd.edu>
2025-08-19 20:44:20 -0700

    Add env JS file

--- node-bin/cgiBinRouter.js
+++ node-bin/cgiBinRouter.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import helloHtmlWorld from './js-helloHtmlWorld.js';
 import helloJsonWorld from './js-hello-json-world.js';
+import envJS from './js-environment.js';
 
 const router = express.Router();
 
 router.use('/js-helloHtmlWorld.js', helloHtmlWorld);        //Any request that starts with /js-helloHtmlWorld.js will be handled by the router defined in helloHtmlWorld.
 router.use('/js-hello-json-world.js', helloJsonWorld); 
+router.use('/js-environment.js', envJS);
 
 
 
---
+++ node-bin/js-environment.js
@@ -0,0 +1,51 @@
+import express from 'express';
+const router = express.Router();
+
+function noHtml(str) {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
+}
+
+
+router.get('/', (req, res) => {
+  res.set('Content-Type', 'text/html; charset=utf-8');
+
+
+    // process.env -> object in Node.js that holds an array of key-value pair environment variables
+    // Object.entries(process.env) -> convert each key-value pair into an array of arrays
+    const env = Object.entries(process.env).sort(([a], [b]) =>
+    a.localeCompare(b) //compare keys
+  );
+
+    
+// Append html
+  let html = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Environment Variables</title>
+    </head>
+    <body>
+        <h1 style="text-align: center;">Environment Variables</h1>
+        <hr>
+  `;
+
+  for (const [key, value] of env) {
+    html += `<b>${noHtml(key)}</b>: ${noHtml(value)}<br> \n`;
+  }
+
+  html += `
+    </body>
+    </html>
+  `;
+
+  res.send(html);
+});
+
+export default router;
